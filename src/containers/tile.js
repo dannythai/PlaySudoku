5cd09a592709e33703e804eeab9989176f646aa6
Danny Thai <dthai92@yahoo.com>
2016-06-11 19:15:35 -0700

    tile turns red on invalid user input

--- src/containers/tile.js
+++ src/containers/tile.js
@@ -7,22 +7,28 @@ import { puzzleAPI } from '../actions/newGame_action';
 class Tile extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {valid: true};
+	}
+
+	validateChange(row, col, val) {
+		var breaksRules = false;
+		if(val !== 0 && !puzzleAPI._checkVal(this.props.activePuzzle, row, col, val)) {
+			this.setState({valid: false});
+		} else {
+			this.setState({valid: true})
+		}
 	}
 
 	onInputChange(value) {
-		const myNum = Number(value);
-		// console.log('value added: ', myNum);
+		const userInput = Number(value);
 		// console.log('solution: ', this.props.solution);
 
 		// validate user input against sudoku rules
 		var row = Math.floor(this.props.index/9);
 		var col = Math.floor(this.props.index % 9);
-		if(!puzzleAPI._checkVal(this.props.activePuzzle, row, col, myNum)) {
-			console.log('ILLEGAL MOVE');
-		}
+		this.validateChange(row, col, userInput);
 
-		this.props.activePuzzle[Number(this.props.index)] = myNum;
+		this.props.activePuzzle[Number(this.props.index)] = userInput;
 		this.props.updateBoard(this.props.activePuzzle);
 		if(JSON.stringify(this.props.activePuzzle) === JSON.stringify(this.props.solutionArr)) {
 			swal({   title: "Sweet!",   text: "Puzzle Complete!",   imageUrl: "../../assets/victorybaby.jpg" });
@@ -33,7 +39,7 @@ class Tile extends Component {
 		if (this.props.solveGame) {
 		  return (<span className="tile">{this.props.solution}</span>);
 		} else if (this.props.val === 0) {
-			return (<span className="tile"><input onChange={event => this.onInputChange(event.target.value)} className="input-box" min="1" max="9" /></span>);
+			return (<span className={this.state.valid ? "tile" : "tile-invalid"}><input onChange={event => this.onInputChange(event.target.value)} className="input-box" min="1" max="9" /></span>);
 		} else {
 			return (<span className="tile">{this.props.val}</span>);
 		}
